60926cc80c53d725bdb88b511868945c86a3fed2
Aditi Parihar <aditiparihar06@gmail.com>
2025-02-03 10:28:33 +0530

    add delete button and its functionality (includes a confirmation prompt)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,6 +36,12 @@ end
         @post = Post.find(params[:id])
     end
 
+     def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+
+        redirect_to posts_path
+     end
 
    private
 
