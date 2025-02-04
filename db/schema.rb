cafe10706577aa89faf1e0dcbb0d912920448db0
Aditi Parihar <aditiparihar06@gmail.com>
2025-02-04 11:38:03 +0530

    create migration for comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20250130063406) do
+ActiveRecord::Schema.define(version: 20250204060022) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
